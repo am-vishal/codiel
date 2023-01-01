93e7b9de9122f77bebe8dbb2c28685a320b81aa9
Vishal Kumar <amvishal09@outlook.com>
2023-01-01 23:27:22 +0530

    added like, create friendship schema, and installed socket.io

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const commentController = require("../controllers/comment_controller");
 
+const commentController = require("../controllers/comment_controller");
 router.post("/create", passport.checkAuthentication, commentController.create);
 router.get("/destroy/:id", passport.checkAuthentication, commentController.destroy);
 
